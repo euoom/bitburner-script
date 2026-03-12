f65847ad3cf6afccdb3aa3cb391f2649b9feac76
euoom <euoom@example.com>
2026-03-12 13:31:15 +0900

    # [Test|수정]: 램 할당량 텍스트 출력 기능 추가 (ram_function.js)

--- euoom/test/ram_function.js
+++ euoom/test/ram_function.js
@@ -11,7 +11,11 @@ export async function main(ns) {
     // 이 구문이 램을 0.1GB 높여서 1.7GB로 만든다면, 함수도 텍스트 패턴 매칭입니다.
     const res = fake.getScriptRam();
     
-    ns.tprint(`Result: ${res}`);
+    // 트릭으로 호출하여 현재 스크립트의 할당량을 출력합니다.
+    const actualRam = ns["getScriptRam"](ns.getScriptName());
+    
+    ns.tprint(`Fake Object Call Result: ${res}`);
+    ns.tprint(`[SYSTEM] Current Script RAM Cost: ${actualRam.toFixed(2)} GB`);
     
     while (true) {
         await ns.sleep(1000);
